e425269ab35e34829f839219946bed294634ba11
えんでばー <pro.onaka.hetta@gmail.com>
2024-01-23 02:55:36 +0900

    Refactor text editing view

--- lib/presentation/views/decoration/common/text_editing_view.dart
+++ lib/presentation/views/decoration/common/text_editing_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:miroru_story_editor/extensions/context_extension.dart';
 import 'package:miroru_story_editor/extensions/string_extension.dart';
 import 'package:miroru_story_editor/model/entities/decorations/text/decoration_text.dart';
 import 'package:miroru_story_editor/model/entities/palette_state/palette_state.dart';
@@ -32,86 +33,92 @@ class TextEditingView extends HookWidget {
     // 関数の中は再度インスタンスを生成する必要がある
     final decorationText = textItem.value.data as DecorationText;
 
-    return Scaffold(
-      backgroundColor: Colors.transparent,
-      body: Stack(
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(4, 4, 4, 0),
-            child: TextToolHeaderView(
-              paletteReducer: paletteReducer,
+    return GestureDetector(
+      onTap: () {
+        context.hideKeyboard();
+      },
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(4, 4, 4, 0),
+              child: TextToolHeaderView(
+                paletteReducer: paletteReducer,
+              ),
             ),
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: IntrinsicHeight(
-              child: Row(
-                children: [
-                  TextSizeSliderView(
-                    onChangeFontSize: (double fontSize) {
-                      if (textItem.value.data is! DecorationText) {
-                        throw Exception(
-                          'textItem.value.data is not DecorationText',
+            Align(
+              alignment: Alignment.centerLeft,
+              child: IntrinsicHeight(
+                child: Row(
+                  children: [
+                    TextSizeSliderView(
+                      onChangeFontSize: (double fontSize) {
+                        if (textItem.value.data is! DecorationText) {
+                          throw Exception(
+                            'textItem.value.data is not DecorationText',
+                          );
+                        }
+                        final newDecorationText =
+                            textItem.value.data as DecorationText;
+                        textItem.value = RenderItem<DecorationText>(
+                          transform: textItem.value.transform,
+                          data: newDecorationText.copyWith(
+                            fontSize: fontSize,
+                          ),
+                          uuid: textItem.value.uuid,
+                          order: textItem.value.order,
                         );
-                      }
-                      final newDecorationText =
-                          textItem.value.data as DecorationText;
-                      textItem.value = RenderItem<DecorationText>(
-                        transform: textItem.value.transform,
-                        data: newDecorationText.copyWith(
-                          fontSize: fontSize,
-                        ),
-                        uuid: textItem.value.uuid,
-                        order: textItem.value.order,
-                      );
-                    },
-                  ),
-                  Align(
-                    child: IntrinsicWidth(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Aa',
-                          hintStyle: TextStyle(
+                      },
+                    ),
+                    Align(
+                      child: IntrinsicWidth(
+                        child: TextField(
+                          autofocus: true,
+                          decoration: InputDecoration(
+                            hintText: 'Aa',
+                            hintStyle: TextStyle(
+                              fontSize: decorationText.fontSize,
+                            ),
+                          ),
+                          style: decorationText.fontFamily.fontStyle.copyWith(
                             fontSize: decorationText.fontSize,
                           ),
                         ),
-                        style: decorationText.fontFamily.fontStyle.copyWith(
-                          fontSize: decorationText.fontSize,
-                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: FontListSelectorWidget(
-                onChangeFontName: (fontName) {
-                  if (textItem.value.data is! DecorationText) {
-                    throw Exception(
-                      'textItem.value.data is not DecorationText',
-                    );
-                  }
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: FontListSelectorWidget(
+                  onChangeFontName: (fontName) {
+                    if (textItem.value.data is! DecorationText) {
+                      throw Exception(
+                        'textItem.value.data is not DecorationText',
+                      );
+                    }
 
-                  final newDecorationText =
-                      textItem.value.data as DecorationText;
-                  textItem.value = RenderItem<DecorationText>(
-                    transform: textItem.value.transform,
-                    data: newDecorationText.copyWith(
-                      fontFamily: fontName,
-                    ),
-                    uuid: textItem.value.uuid,
-                    order: textItem.value.order,
-                  );
-                },
+                    final newDecorationText =
+                        textItem.value.data as DecorationText;
+                    textItem.value = RenderItem<DecorationText>(
+                      transform: textItem.value.transform,
+                      data: newDecorationText.copyWith(
+                        fontFamily: fontName,
+                      ),
+                      uuid: textItem.value.uuid,
+                      order: textItem.value.order,
+                    );
+                  },
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
